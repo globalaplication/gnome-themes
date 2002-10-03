f631921e3f11648b657e7d7a04858841f78abb5d
Calum Benson <calum@src.gnome.org>
2002-10-03 12:51:41 +0000

    Move creation of pixmaps/Makefile.am and gtk-2.0/gtkrc.in to autogen.

--- autogen.sh
+++ autogen.sh
@@ -24,6 +24,17 @@
 echo "Generating configuration files for gnome-themes, please wait...."
 echo;
 
+for theme in LargePrint HighContrast HighContrastLargePrint HighContrastInverse HighContrastLargePrintInverse LowContrast LowContrastLargePrint;  do
+	touch $theme/gtk-2.0/gtkrc.in
+	cat $theme/gtk-2.0/$theme.rc common/gtk-stock-icons.rc common/panel-stock-icons.rc common/media-stock-icons.rc > $theme/gtk-2.0/gtkrc.in
+done
+
+for theme in LargePrint HighContrastLargePrint HighContrastLargePrintInverse LowContrastLargePrint; do
+	touch $theme/pixmaps/Makefile.am
+	cat common/all-stock-icons.data $theme/pixmaps/$theme.makefile > $theme/pixmaps/Makefile.am
+
+done
+
 #echo n | gettextize --copy --force;
 aclocal $ACLOCAL_FLAGS;
 autoheader;
