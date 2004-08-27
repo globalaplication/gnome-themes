717c9516ca6110a769d5d804656b16abc0a6cbe9
Calum Benson <calum@src.gnome.org>
2004-08-27 16:36:19 +0000

    Removed erroneous lines that I was using testing...

--- autogen.sh
+++ autogen.sh
@@ -30,9 +30,6 @@ PKG_NAME=gnome-themes
 REQUIRED_AUTOMAKE_VERSION=1.7
 USE_GNOME2_MACROS=1
 
-AUTOMAKE=automake-1.7
-ACLOCAL=aclocal-1.7
-
 if [ ! -f "$srcdir/gnome-themes.spec.in" ]; then
  echo "$srcdir doesn't look like source directory for $PKG_NAME" >&2
  exit 1
